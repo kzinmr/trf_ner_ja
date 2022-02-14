b7e1a20d6515827de011eb7f720a5d49de44ab51
Kazuki Inamura <kzinmr109@gmail.com>
2022-02-14 22:38:00 +0900

    fix

--- convert_span_data.py
+++ convert_span_data.py
@@ -6,8 +6,11 @@ assert input_filename.endswith(".jsonl")
 with open(input_filename) as fp:
     jds = [json.loads(l) for l in fp if l.strip()]
 span_dataset = [
-    {"text": text, "spans": [{"start": s, "end": e, "label": l} for s, e, l in ents]}
-    for text, ents in jds
+    {
+        "text": text,
+        "spans": [{"start": s, "end": e, "label": l} for s, e, l in entd["entities"]],
+    }
+    for text, entd in jds
 ]
 
 output_filename = input_filename + ".new.jsonl"
