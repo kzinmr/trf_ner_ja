1c5d49a284970a3ee7dc0c63795a90f776baf895
Kazuki Inamura <kzinmr109@gmail.com>
2022-02-14 22:36:34 +0900

    add: conversion for camphr format

---
+++ convert_span_data.py
@@ -0,0 +1,18 @@
+import json
+import sys
+
+input_filename = sys.argv[1]
+assert input_filename.endswith(".jsonl")
+with open(input_filename) as fp:
+    jds = [json.loads(l) for l in fp if l.strip()]
+span_dataset = [
+    {"text": text, "spans": [{"start": s, "end": e, "label": l} for s, e, l in ents]}
+    for text, ents in jds
+]
+
+output_filename = input_filename + ".new.jsonl"
+with open(output_filename, "wt") as fp:
+    for d in span_dataset:
+        js = json.dumps(d, ensure_ascii=False)
+        fp.write(js)
+        fp.write("\n")
