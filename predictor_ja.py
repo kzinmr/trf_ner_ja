0991e2d04436dbc25c27f9f46da536c2b5425820
Kazuki Inamura <kzinmr109@gmail.com>
2022-02-17 21:14:05 +0900

    fix

--- predictor_ja.py
+++ predictor_ja.py
@@ -181,6 +181,7 @@ class SlowEncoder:
         NOTE: 長文の対策として、ストライド付きウィンドウ分割も行う。
         """
         # sentence -> [sentence_window(max_length, window_stride)]
+        assert self.window_stride < self.max_length - 2
         words_in_windows = self.pretokenizer.window_by_tokens(
             sentence_text, self.max_length - 2, self.window_stride
         )
@@ -225,7 +226,7 @@ class SlowEncoder:
             word_ids = self.align_tokens_with_words(words_str, _tokens)
             # NOTE: window内->元文内の位置スパン、トークンでなく単語単位の位置スパンを登録
             word_spans = [(w.start, w.end) for w in words]
-            window_offset = n_stride * self.window_stride
+            window_offset = (self.max_length - 2 - n_stride) * self.window_stride
             token_word_labels = []
             for tok, wid in zip(_tokens, word_ids):
                 word_text = words_str[wid]
