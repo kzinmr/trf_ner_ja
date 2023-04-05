804e325358195e69b9b0a55112d96b93a47a802b
Kazuki Inamura <kzinmr109@gmail.com>
2023-04-05 15:51:18 +0900

    mv

--- convert_camphr_format.py
+++ convert_camphr_format.py
@@ -5,12 +5,18 @@ input_filename = sys.argv[1]
 assert input_filename.endswith(".jsonl")
 with open(input_filename) as fp:
     jds = [json.loads(l) for l in fp if l.strip()]
+    jds = [
+        (text, sorted({(s, e, l) for s, e, l in entd["entities"]}, key=lambda x: x[0]))
+        for text, entd in jds
+    ]
+
 span_dataset = [
     {
         "text": text,
-        "spans": [{"start": s, "end": e, "label": l} for s, e, l in entd["entities"]],
+        "spans": [{"start": s, "end": e, "label": l} for s, e, l in ents],
     }
-    for text, entd in jds
+    for text, ents in jds
+    if len(text) > 5
 ]
 
 output_filename = input_filename + ".new.jsonl"
